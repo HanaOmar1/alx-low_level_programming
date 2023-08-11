2c3b3435567b34a6846b2340af7216f1f424cb1d
HanaOmar1 <Hanaomar20043@gmail.com>
2023-08-11 00:19:09 +0000

    r

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  * Description: A c program that prints with put function 
  *
  * Return: Alawys (0) (success)
- */
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");

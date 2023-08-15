64268ea03e8afbf9449fe3f462152f5cbdcdcdc1
HanaOmar1 <Hanaomar20043@gmail.com>
2023-08-15 16:42:11 -0700

    k

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,7 +12,7 @@ for (min = 0; min <= 59; min++)
 {
 _putchar((hrs / 10) + 48);
 _putchar((hrs % 10) + 48);
-_putchar(' : ');
+_putchar(':');
 _putchar((min / 10) + 48);
 _putchar((min % 10) + 48);
 _putchar('\n');

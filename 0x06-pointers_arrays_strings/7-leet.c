5327210b568c8de0c90b02410e9a3f13f356a262
HanaOmar1 <Hanaomar20043@gmail.com>
2023-08-24 16:06:12 -0700

    l

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,27 +3,24 @@
 
 /**
  *  leet - encodes a string into 1337
- * @c: string
+ * @n: input value
  *
- *  Return: string that is encoded
+ *  Return: n value
 */
-char *leet(char *c)
+char *leet(char *n)
 {
-char *cp = c;
-char key[] = {'A', 'E', 'O', 'T', 'L'};
-int value[] = {4, 3, 0, 7, 1};
-unsigned int i;
-while (*c)
+int i, j;
+char s1[] = "aAeEoOtTlL";
+char s2[] = "4433007711";
+for (i = 0; n[i] != '\0'; i++)
 {
-for (i = 0; i < sizeof(key) / sizeof(char); i++)
+for (j = 0; j < 10; j++)
 {
-if (*c == key[i] || *c == key[i] + 32)
+if (n[i] == s1[j])
 {
-*c = 48 + value[i];
+n[i] = s2[j];
 }
-
 }
-c++
 }
-return (cp);
+return (n);
 }

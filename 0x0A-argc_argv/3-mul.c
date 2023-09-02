1d6e818a2b92afb3a69d2903209832f567a6c2a2
HanaOmar1 <Hanaomar20043@gmail.com>
2023-09-02 22:17:52 +0000

    l

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,40 +1,32 @@
 #include <stdio.h>
 /**
  * _atoi- converts a string to an integer
- * @s: string to be converted
+ * @str: string to be converted
  * Return: the int converted from string
  */
-int _atoi(char *s)
+int _atoi(const char *str)
 {
-int i, d, n, len, f, digit;
-i = 0;
-d = 0;
-n = 0;
-len = 0;
-f = 0;
-digit = 0;
-while (s[len] != '\0')
-len++;
-while (i < len && f == 0)
+int result = 0;
+int sign = 1;
+int i = 0;
+
+if (str[0] == '-')
 {
-if (s[i] == '-')
-++d;
-if (s[i] >= '0' && s[i] <= '9')
-{
-digit = s[i] - '0';
-if (d % 2)
-digit = -digit;
-n = n * 10 + digit;
-f = 1;
-if (s[i +1] < '0' || s[i +1] > '9')
-break;
-f = 0;
+sign = -1;
+i = 1;
 }
-i++;
-}
-if (f == 0)
+
+for (; str[i] != '\0'; i++)
+{
+if (str[i] < '0' || str[i] > '9')
+{
 return (0);
-return (n);
+}
+
+result = result * 10 + str[i] - '0';
+}
+
+return (result *sign);
 }
 
 /**
